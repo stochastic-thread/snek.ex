4cbf659ca2d874e749571fae5ca59f36400cf3fb
stochastic-thread <hello@arthurcolle.com>
2018-10-12 00:40:25 -0400

    Refactor into cmd command

--- lib/snake.ex
+++ lib/snake.ex
@@ -1,25 +1,18 @@
 defmodule Snake do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
-
-    children = [
-      # Define workers and child supervisors to be supervised
-      # worker(Snake.Worker, [arg1, arg2, arg3]),
-    ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
+    children = []
     opts = [strategy: :one_for_one, name: Snake.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  def a do
-    {:ok, pp} = :python.start()
-    :python.call(pp, :sys, String.to_atom("version.__str__"), [])
+
+  def cmd(:vzn) do
+    cmds = [ vzn: "version.__str__" ]
+    {:ok, p} = :python.start()
+    :python.call(p, :sys, String.to_atom(Keyword.get(cmds, :vzn)), [])
   end
 
   def b do
